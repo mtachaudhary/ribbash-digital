3d718ce61e0ab46371a27dff254f78f93f3491b3
mtachaudhary <tayyab.chaudhary786@gmail.com>
2020-06-29 17:53:18 +0500

    Updated portfolio pages, developing Our Services -  UI/UX page

--- js/custom-script.js
+++ js/custom-script.js
@@ -26,6 +26,31 @@
         }
     });
 
+    // Initialize Related Products owl-carousel
+    $('.rd-related-products-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        navText: ['<i class="fas fa-arrow-left"></i>', '<i class="fas fa-arrow-right"></i>'],
+        responsive:{
+            0:{
+                items: 1,
+                nav: false,
+                dots: true
+            },
+            600:{
+                items: 2,
+                nav: false,
+                dots: true
+            },
+            1000:{
+                items: 2,
+                nav: true,
+                dots: false
+            }
+        }
+    });
+
     // Setup wow.js
     new WOW().init();
 
