fe70c1701a36e99eae3b2ff1fbc42c0194c3855a
mtachaudhary <tayyab.chaudhary786@gmail.com>
2020-08-06 22:58:32 +0500

    fixed header visibility issue on about us, contact us, and jobs pages, also added email functionality

---
+++ includes/contact-form-handler.php
@@ -0,0 +1,31 @@
+<?php
+
+$email = $_POST["email"];
+$name = $_POST["full-name"];
+$website = $_POST["website"];
+$message = $_POST["message"];
+
+$to = "hello@ribbash.com";
+$subject = "Ribbash Digital Contact Form";
+
+$headers  = "From: $name <$email>\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+//Set the content-type to html
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$body  = "<html><body>";
+$body .= "<b>Name: </b>$name <br/>
+<b>Email: </b>$email <br/>
+<b>Website: </b>$website <br/>
+<b>Message: </b>$message";
+$body .= "</body></html>";
+
+
+if (mail($to, $subject, $body, $headers)) {
+    //redirect to the 'thank you' page
+    header('Location: ../index.html');
+} else {
+    echo "Email sending failed...";
+}
+
+?>
